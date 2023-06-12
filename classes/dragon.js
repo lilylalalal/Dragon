521be05d34f4e4121fb28499493e459accc63da8
Lily Au <lilyau-sp@Lilys-MacBook-Pro.local>
2023-06-12 12:01:48 +0100

    added name and color arguments into Dragon Class.  Instance Method of breathFire() is added.  static method getDrgaon(...Dragon) is added which can return a list of Dragon names.

--- classes/dragon.js
+++ classes/dragon.js
@@ -1,5 +1,31 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name
+    this.color =color
+  }
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`
+  }
+  static getDragons(...dragonclasses){
+    let list = []
+    dragonclasses.forEach((dragon)=>{ 
+      list.push(dragon.name)})
+      
+    return list
 
+  }
+}
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
+const toothless = new Dragon("Toothless", "black");
+console.log(toothless);
+console.log(toothless.breathesFire());
+
+console.log(Dragon.getDragons(puff, toothless));
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
